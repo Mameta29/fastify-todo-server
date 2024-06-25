2b7694078048967fa7a52d6cc13bc49b06de7160
Mameta29 <fc.kikkawa@gmail.com>
2024-06-25 20:31:48 +0900

    feature/middleware ログインチェックmiddleware作成

--- routes.ts
+++ routes.ts
@@ -2,8 +2,10 @@ import { FastifyWrapper } from './_wrapper';
 import { TodoController } from './app/controllers/TodoController';
 import { UserController } from './app/controllers/UserController';
 import { AuthController } from './app/controllers/AuthController';
+import { loginCheckMiddleware } from './app/middlewares/loginCheckMiddleware';
 
 export const registerRoutes = (route: FastifyWrapper) => {
+  route.middleware('/todos', ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], loginCheckMiddleware, {});
   // Todo routes
   route.get('/todos', TodoController.getTodos);
   route.post('/todos', TodoController.createTodo);
