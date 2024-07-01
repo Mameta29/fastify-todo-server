dd87f6272c1549d6720c4dfcdec2a28104d23b34
Mameta <102901017+Mameta29@users.noreply.github.com>
2024-07-01 13:56:55 +0900

    seeder追加 (#23)
    Co-authored-by: Shinei Kikkawa <shineikikkawa@Shineis-MacBook-Air.local>

--- app/types.ts/types.ts
+++ app/types.ts/types.ts
@@ -1,28 +1,27 @@
 import type { ColumnType } from "kysely";
-export type Generated<T> =
-  T extends ColumnType<infer S, infer I, infer U>
-    ? ColumnType<S, I | undefined, U>
-    : ColumnType<T, T | undefined, T>;
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
 export type Todo = {
-  id: Generated<number>;
-  title: string;
-  content: string;
-  status: Generated<number>;
-  createdAt: Generated<Timestamp>;
-  updatedAt: Timestamp;
-  userId: number;
+    id: Generated<number>;
+    title: string;
+    content: string;
+    status: Generated<number>;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
+    userId: number;
 };
 export type User = {
-  id: Generated<number>;
-  username: string;
-  email: string;
-  password: string;
-  createdAt: Generated<Timestamp>;
-  updatedAt: Timestamp;
+    id: Generated<number>;
+    username: string;
+    email: string;
+    password: string;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
 };
 export type DB = {
-  Todo: Todo;
-  User: User;
+    Todo: Todo;
+    User: User;
 };
